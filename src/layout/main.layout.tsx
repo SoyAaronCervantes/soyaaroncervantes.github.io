4da56c442fe315623eb9279fc688964f943e8892
Aaron Cervantes <soy.aaron.cervantes@gmail.com>
2022-12-04 10:54:51 -0600

    Initial commit

---
+++ src/layout/main.layout.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import {ProfileComponent} from "../components/profile.component";
+export const MainLayout = () => {
+    return (
+        <main className="[ main ]">
+            <ProfileComponent/>
+        </main>
+    );
+};
